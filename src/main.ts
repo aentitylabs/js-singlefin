645a2026bd6a9e86f12392bd0a1ece5ede08a858
Edoardo Agudio <edoardoagudio@gmail.com>
2021-12-21 15:45:56 +0100

    fix: fix costants

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,9 @@ import { State } from "./influencer/state";
 import { Modules } from "./modules";
 import { HtmlTemplateEngine } from "js-html-template-engine";
 
-declare global {
-    var SINGLEFIN_APP_NAME: any;
-    var SINGLEFIN_MODEL: any;
-}
-
-global.SINGLEFIN_APP_NAME = SINGLEFIN_APP_NAME;
-global.SINGLEFIN_MODEL = SINGLEFIN_MODEL;
-
 declare const SINGLEFIN_APP_NAME: string;
 declare const SINGLEFIN_TRENDS: any;
-//declare const SINGLEFIN_MODEL: any;
+declare const SINGLEFIN_MODEL: any;
 declare const SINGLEFIN_PAGES: any;
 declare const SINGLEFIN_PAGES_COMPONENTS: any;
 
